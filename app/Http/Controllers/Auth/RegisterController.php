3bbc571587cf17c8a8ff2bafb6a4c4f9f530e922
Phintos <carlopinto@ied.edu>
2018-05-12 18:45:16 +0200

    Login
    Aggiunto login

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,6 +52,8 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+            'gender' => 'required|string',
+            'location' => 'required|string',
         ]);
     }
 
@@ -67,6 +69,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'gender' => $data['gender'],
+            'location' => $data['location'],
         ]);
     }
 }
